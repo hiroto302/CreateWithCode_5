3deb330c799b3ab453e54441e867902b83bbb21b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-23 12:34:52 +0900

    Create TargetObjects & Target.cs

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float _minSpeed = 12, _maxSpeed = 16 ;
+    private float _maxTorque = 10;
+    private float _xRange = 4, _ySpawnPos = -6;
+    Rigidbody _targetRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Creates a new vector with given x, y components and sets z to zero. So, z is always zero.
+        transform.position = RandomSpawnPos();
+
+        _targetRb = GetComponent<Rigidbody>();
+        _targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        _targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(_minSpeed, _maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-_maxTorque, _maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-_xRange, _xRange), _ySpawnPos);
+    }
+
+
+}
